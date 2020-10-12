890d8890e99a59a3c1a3a445c7be3cf871f91588
JLARTEY <ANGELJULIA.LARTEY125@GMAIL.COM>
2020-10-12 13:00:28 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
+
+const BASE_URL = "https://api.exchangeratesapi.io/latest"
+
 function App() {
+
+  const [currencyOptions, setCurrencyOptions]=useState([])
+  console.log(currencyOptions)
+  const[fromCurrency,setFromCurrency]= useState()
+  const[toCurrency, setToCurrency] =useState()
+  const[amount, setAmount]= useState(1)
+  const[amountInfromCurrency,setaAmountInFromCurrency]=useState(true)
+
+
+  useEffect(() => {
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(data => {
+      const firstCurrency =Object.keys(data.rates)[0]
+      setCurrencyOptions([data.base,...Object.keys(data.rates)])
+      setFromCurrency(data.base)
+      setToCurrency(firstCurrency)
+      })
+     
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>convert</h1>
+      <currencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency= {fromCurrency}
+        
+      />
+      <h3>
+      <input type="number" />
+        <select>
+          {currencyOptions.map(option => (
+            <option key={option} value ={option}>{option}</option> 
+          ))}
+      </select>
+      </h3>
+
+      <div className="equal">=</div>
+      <currencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+      />
+
+      <h4>
+        <input type="number" />
+        <select>
+          {currencyOptions.map(option => (
+          <option key={option} value={option}>{option}</option>
+          ))}
+        </select>
+      </h4>
+   </>
   );
 }
 
---
+++ src/CurrencyRow.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function CurrencyRow(props) {
+    const {
+        currencyOptions,
+        selectedCurrency
+    } = props
+    return (
+        <div>
+           <input type="number" className="input"/>
+           <select value={selectedCurrency} >
+               {currencyOptions.map(option => (
+                   <option key={option}value= {option}>{option}</option>
+               ))}
+               
+           </select>
+        </div>
+        
+    )
+}
+
+export default CurrencyRow
